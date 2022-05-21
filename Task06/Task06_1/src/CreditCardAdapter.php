e66245e323e64447cda14052ca4c16626148be00
Nastyalover <101454549+Nastyalover@users.noreply.github.com>
2022-05-21 15:12:48 +0300

    Task06

---
+++ Task06/Task06_1/src/CreditCardAdapter.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Chulkina\Task06_1;
+
+class CreditCardAdapter implements PaymentAdapterInterface
+{
+    private $adaptee;
+
+    public function __construct(CreditCard $adaptee)
+    {
+        $this -> adaptee = $adaptee;
+    }
+
+    public function collectMoney($amount)
+    {
+        return $this -> adaptee -> authorizeTransaction();
+    }
+}
---
+++ Task06/Task06_2/src/RoomInterface.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Chulkina\Task06_2;
+
+interface RoomInterface
+{
+    public function getPrice(): int;
+    public function getDescription(): string;
+}
